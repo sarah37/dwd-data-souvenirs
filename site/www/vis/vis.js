941d3df9c2cf535e7cc9f476b8394db1893d519d
Xin Wen <Saaam030@users.noreply.github.com>
2019-03-25 20:19:34 +0000

    vis improved

--- site/www/vis/vis.js
+++ site/www/vis/vis.js
@@ -1,15 +1,17 @@
 function drawVis(events) {
 
+	console.log(events)
+
 	// set min and max date
 	var thisyear = 2018
 	var parseDate = d3.timeParse("%d/%m/%Y");
 	var minDate = parseDate('01/08/' + thisyear)
-	var maxDate = parseDate('15/08/' + thisyear)
+	var maxDate = parseDate('31/08/' + thisyear)
 
 	// create time scale (vertical axis)
 	var timeScale = d3.scaleTime()
 		.domain([minDate,maxDate])  // data space
-		.range([0, 500]);  // display space
+		.range([0, 1000]);  // display space
 
 	// projection
 	const s = 130000
@@ -27,6 +29,12 @@ function drawVis(events) {
 	camera.position.y = 0
 	camera.position.z = 1100
 
+	//SET UP LIGHT
+	var light = new THREE.DirectionalLight( 0xffffff,2 );
+	light.position.set( 100, 100, 100 ).normalize();
+	scene.add( light );
+
+
 	// initialize camera plugins to drag the camera
 	var orbitControls = new THREE.OrbitControls(camera)
 	orbitControls.autoRotate = true
@@ -35,6 +43,8 @@ function drawVis(events) {
 	var renderer = new THREE.WebGLRenderer();
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	document.getElementById('section-4').appendChild( renderer.domElement );
+
+	var clock = new THREE.Clock();
 
 	////////////////////////////////////////////////////////////////////////////////
 	//draw plane w/ map pictures
@@ -67,58 +77,63 @@ function drawVis(events) {
 	})
 
 
-    // add cubes and add small nebula around each cube (?)
-    var cubes = []
+    // add stars and satellites
+	var objects = [];
+	var sats=[];
     events.forEach(function(ev) {
     	ev.pos = projection([ev.longitude, ev.latitude])
-    	var geometry = new THREE.BoxGeometry( 20, 20, 20 );
-		var material = new THREE.MeshNormalMaterial();
-		var cube = new THREE.Mesh( geometry, material );
-		cube.position.x = ev.pos[0]-512 //front-back
-		cube.position.z = ev.pos[1]-512 //left-right
-		if(timeScale(d3.isoParse(ev.performances[0].start))>-500 && timeScale(d3.isoParse(ev.performances[0].start))<500) {
-			cube.position.y = timeScale(d3.isoParse(ev.performances[0].start))-200//vertical
-			cubes.push(cube);
-			scene.add( cube );
-			var geometry = new THREE.Geometry()
-			var material = new THREE.PointsMaterial({
-				size: 6,
-				vertexColors: true, // 是否为几何体的每个顶点应用颜色，默认值是为所有面应用材质的颜色
-				color: 0xffffff
+
+		var geometry = new THREE.OctahedronGeometry(20, 0);
+		var material = new THREE.MeshLambertMaterial({
+			color: Math.random() * 0x31f3ff,wireframe:false
+		})
+		var sphere = new THREE.Mesh( geometry, material );
+		sphere.position.x = ev.pos[0]-512 //front-back
+		sphere.position.z = ev.pos[1]-512 //left-right
+		if(timeScale(d3.isoParse(ev.performances[0].start))>-600 && timeScale(d3.isoParse(ev.performances[0].start))<1000) {
+			sphere.position.y = timeScale(d3.isoParse(ev.performances[0].start)) - 200//vertical
+			objects.push(sphere);
+			scene.add(sphere);
+
+
+			var labelDiv = document.createElement('div');
+			labelDiv.className = 'label';
+			labelDiv.textContent = ev.title;
+			labelDiv.style.marginTop = '-1em';
+
+			var Label = new THREE.CSS2DObject(labelDiv);
+			Label.position.set(0, 50, 0);
+			sphere.add(Label);
+
+			labelRenderer = new THREE.CSS2DRenderer();
+			labelRenderer.setSize( window.innerWidth, window.innerHeight );
+			labelRenderer.domElement.style.position = 'absolute';
+			labelRenderer.domElement.style.top = 0;
+			labelRenderer.domElement.style.opacity = '0.5';
+			labelRenderer.domElement.style.fontFamily = 'Roboto,sans-serif';
+			labelRenderer.domElement.style.fontWeight = '100';
+			document.body.appendChild( labelRenderer.domElement );
+
+			var satgeometry = new THREE.TetrahedronGeometry(5, 0);
+			var satmaterial = new THREE.MeshBasicMaterial({
+				color: 0xfff001,wireframe:false
 			})
+			var  sat = new THREE.Mesh( satgeometry, satmaterial );
+			sat.position.set(Math.random() * 50, 0, Math.random() * 50);
 
-
-			for ( var i = 0; i < cubes.length; i++){
-
-				for (var x = 0; x < 2; x++) {
-					for (var y = 0; y < 2; y++) {
-						for (var z = 0; z < 2; z++) {
-							var particle = new THREE.Vector3(
-								cubes[i].position.x - Math.random() * 30 + Math.random() * 20 + 10,
-								cubes[i].position.y - Math.random() * 30+Math.random() * 20 + 10,
-								cubes[i].position.z - Math.random() * 30+Math.random() * 20 + 10)
-
-							geometry.vertices.push(particle)
-							geometry.colors.push(new THREE.Color(Math.random() * 0xffffff))
-						}
-					}
-				}
-				var nebula = new THREE.Points(geometry, material)
-				// console.log(nebula)
-				scene.add(nebula)
-
-				}
+			sphere.add(sat);
+			sats.push(sat);
 			}
 
 	    })
 
 	    // Create a closed wavey loop
 	    var curvespositions = [];
-	    cubes.forEach(function(cu) { curvespositions.push(cu.position) })
+	    objects.forEach(function(cu) { curvespositions.push(cu.position) })
 	    var curve = new THREE.CatmullRomCurve3(curvespositions);
 	    var points = curve.getPoints( events.length * 10 );
 	    var geometry = new THREE.BufferGeometry().setFromPoints( points );
-	    var material = new THREE.LineBasicMaterial( { color : 0xcccccc } );
+	    var material = new THREE.LineDashedMaterial( { color: 0xffffff,dashSize: 20, gapSize: 5 } );
 		// 线的材质可以由2点的颜色决定
 		// var material = new THREE.LineBasicMaterial( { vertexColors: true } );
 		// var color1 = new THREE.Color( 0x444444 ), color2 = new THREE.Color( 0xFF0000 );
@@ -127,41 +142,15 @@ function drawVis(events) {
 		// geometry.colors.push( color1, color2 );
 	    // Create the final object to add to the scene
 	    var curveObject = new THREE.Line( geometry, material );
+	    curveObject.computeLineDistances();
 	    scene.add(curveObject);
 	// })
 
-	//Create Decoration
-	var group;
-	var allCubes=[];
-	group = new THREE.Group();
-	scene.add( group );
-	//purple color point
-	var geometry = new THREE.Geometry()
-	new THREE.MeshNormalMaterial(1,1,1)
-	var material = new THREE.PointsMaterial({
-		size: 3,
-		vertexColors: true, // 是否为几何体的每个顶点应用颜色，默认值是为所有面应用材质的颜色
-	})
-
-	for (var x = -3; x < 3; x++) {
-		for (var y = -3; y < 3; y++) {
-			for(var z = -3; z < 3; z++){
-				var particle = new THREE.Vector3(
-					Math.random() * 300 - 150,
-					Math.random() * 150 - 75,
-					Math.random() * 300 - 150)
-				geometry.vertices.push(particle)
-				geometry.colors.push(new THREE.Color(0x996cef))
-			}
-		}
-	}
-	var points2 = new THREE.Points(geometry, material)
-
 	// light
-	var light = new THREE.AmbientLight( 0x404040 ); // soft white light
-	scene.add( light );
+	// var light = new THREE.AmbientLight( 0x404040 ); // soft white light
+	// scene.add( light );
 
-	//white points1
+	//white points at the corners
 	var geometry1= new THREE.Geometry()
 	var material2 = new THREE.PointsMaterial({
 		size: 5,
@@ -169,70 +158,61 @@ function drawVis(events) {
 		color: 0xe8e8e8
 	})
 
-	for (var x = -3; x < 3; x++) {
-		for (var y = -3; y < 3; y++) {
-			for(var z = -3; z < 3; z++) {
+	for (var x = 0; x <2 ; x++) {
+		for (var y = 0; y < 2; y++) {
+			for(var z = 0; z < 2; z++) {
 				var particle = new THREE.Vector3(
-					Math.random() * 1100 - 550,
-					Math.random() * 1100 - 550,
-					Math.random() * 1100 - 550)
+					x*1000-500,
+					y*1000-500,
+					z*1000-500)
 				geometry1.vertices.push(particle)
 				geometry1.colors.push(new THREE.Color(0xe8e8e8))
 			}
 		}
 	}
+
 	var wpoints1 = new THREE.Points(geometry1, material2)
+	console.log(wpoints1);
+	scene.add(wpoints1);
 
 	// To make sure that the matrixWorld is up to date for the boxhelpers
 	var sphere = new THREE.SphereGeometry();
-	var object = new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( 0xff0000 ) );
-	var box = new THREE.BoxHelper(wpoints1, 0x898989 );
+	var object = new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( 0x141414 ) );
+	var box = new THREE.BoxHelper(wpoints1, 0x565656  );
 	scene.add( box );
 
 
-	// 生成纹理
-	function generateSprite() {
-
-		var canvas = document.createElement('canvas');
-		canvas.width = 100;
-		canvas.height = 100;
-
-		var context = canvas.getContext('2d');
-		var gradient = context.createRadialGradient(canvas.width / 2, canvas.height / 2, 0, canvas.width / 2, canvas.height / 2, canvas.width / 2);
-		gradient.addColorStop(0, 'rgba(255,255,255,1)');
-		gradient.addColorStop(0.2, 'rgba(255,255,255,1)');
-		gradient.addColorStop(0.4, 'rgba(255,255,255,1)');
-		gradient.addColorStop(1, 'rgba(255,255,255,1)');
-
-		context.fillStyle = gradient;
-		context.fillRect(0, 0, canvas.width, canvas.height);
-
-		var texture = new THREE.Texture(canvas);
-		texture.needsUpdate = true;
-		return texture;
-
-	}
-
-
-	var step = 0
+	// var step = 0
 
 	// animate/render loop
 	var animate = function () {
 		requestAnimationFrame( animate );
-	   //background color
-		renderer.setClearColor(0x000000,1.0);
+		renderer.setClearColor(0x232323,1.0);
+		for(var i = 0; i < sats.length;i++) {
+
+			var elapsed = clock.getElapsedTime();
+			sats[i].position.set(Math.sin( elapsed ) * 50 , 0 ,Math.cos( elapsed ) * 50);
+
+			sats[i].rotation.x += 0.001;
+			sats[i].rotation.y += 0.001;
+		}
+		//background color
 		// cube.rotation.x += 0.01;
 		// cube.rotation.y += 0.01;
-		step += 0.0001;
-		points2.rotation.y -= 0.0006;
+		// step += 0.0001;
+		// points2.rotation.y -= 0.0006;
 		//points3.rotation.y -=0.0003;
-		wpoints1.rotation.x = step;
-		wpoints1.rotation.y = step;
-		wpoints1.rotation.z = step;
+		// wpoints1.rotation.x = step;
+		// wpoints1.rotation.y = step;
+		// wpoints1.rotation.z = step;
+
+
 		renderer.render( scene, camera );
+		labelRenderer.render( scene, camera );
 
 	};
 
 	animate();
+
 
 }

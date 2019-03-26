e5f290c224882bc1880d0d9cb68064d9b86481f0
Leona_z <adjmeme@users.noreply.github.com>
2019-03-26 15:17:25 +0000

    Revert "labelcontrol"
    This reverts commit 294744f2ad265c7381e6a69dea91d0c6baf8c534.

--- site/select-vis/vis/vis.js
+++ site/select-vis/vis/vis.js
@@ -80,7 +80,6 @@ function drawVis(events) {
     // add stars and satellites
 	var objects = [];
 	var sats=[];
-  var Labels=[];
     events.forEach(function(ev) {
     	ev.pos = projection([ev.longitude, ev.latitude])
 
@@ -96,6 +95,7 @@ function drawVis(events) {
 			objects.push(sphere);
 			scene.add(sphere);
 
+
 			var labelDiv = document.createElement('div');
 			labelDiv.className = 'label';
 			labelDiv.textContent = ev.title;
@@ -103,10 +103,7 @@ function drawVis(events) {
 
 			var Label = new THREE.CSS2DObject(labelDiv);
 			Label.position.set(0, 50, 0);
-      //sphere.add(Label);
-			Labels.push(Label);
-
-
+			sphere.add(Label);
 
 			labelRenderer = new THREE.CSS2DRenderer();
 			labelRenderer.setSize( window.innerWidth, window.innerHeight );
@@ -184,43 +181,8 @@ function drawVis(events) {
 	var box = new THREE.BoxHelper(wpoints1, 0x565656  );
 	scene.add( box );
 
-	//grid Helper
-  var gridHelper = new THREE.GridHelper(1000, 50, 0xFF4444, 0x404040 );
-  scene.add( gridHelper );
-	//GUI for gridhelper
-  var gui=new dat.GUI();
-  var controls = new function(){
-    this.gridHelper =false;
-    this.label=true;
-  }
-  var allGui=gui.addFolder('Control');
-  allGui.add(controls,'gridHelper').onChange(function(e){
-  console.log(e);
-  if(e){
-    scene.remove(gridHelper);
-  }else{
-    scene.add(gridHelper);
-  }
-  });
-  //GUI for label
 
-  for(var i=0;i<objects.length;i++){
-    objects[i].add(Labels[i]);
-  }
-  allGui.add(controls,'label').onChange(function(e){
-    if(e){
-      for(var i=0;i<objects.length;i++){
-        objects[i].add(Labels[i]);
-      }
-    }else{
-      for(var i=0;i<objects.length;i++){
-        objects[i].remove(Labels[i]);
-      }
-    }
-  });
-
-
-	// // var step = 0
+	// var step = 0
 
 	// animate/render loop
 	var animate = function () {
@@ -246,9 +208,9 @@ function drawVis(events) {
 
 
 		renderer.render( scene, camera );
-
 		labelRenderer.render( scene, camera );
-  };
+
+	};
 
 	animate();
 
